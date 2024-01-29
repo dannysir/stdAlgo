8521439566c2fa92d8284bc7c04fc7a79565e7b3
Seo San <dannysir@naver.com>
2024-01-29 14:56:03 +0900

    백준 1012 해결

--- baek/1012.js
+++ baek/1012.js
@@ -1,72 +1,62 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.text").toString().trim().split("\n");
-//let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-
-let testNUM = input.shift();
+let N = parseInt(input.shift());
 
 
-function main(changedinput) {
-    let inputSan = changedinput;
-    let map;
-    let visited;
-    let maxX = 0;
-    let maxY = 0;
+// const BFS = (nowX, nowY) => {
+//     let LocQueue = [];
+//     let dx = [1, -1, 0, 0];
+//     let dy = [0, 0, 1, -1];
+//     LocQueue.push([nowX, nowY]);
+//     while (LocQueue.length) {
+//         const [X, Y] = LocQueue.shift();
+//         map[X][Y] = 0;
+//         for (let i = 0; i < dx.length; i++) {
+//             const nextX = X + dx[i];
+//             const nextY = Y + dy[i];
+//             if (nextY >= SIZE[1] || nextX >= SIZE[0] || nextX <0 || nextY < 0) continue;
+//             if (map[nextX][nextY] === 1) {
+//                 LocQueue.push([nextX, nextY]);
+//             }
+//         }
+//     }
+// };
 
-    function makeMap(arr) {
-        let all = arr.shift().split(' ');
-        maxX = parseInt(all[0]);
-        maxY = parseInt(all[1]);
-        visited = new Array(maxX);
-        map = new Array(maxX);
-        for (let i = 0; i < visited.length; i++) {
-            visited[i] = new Array(maxY).fill(false);
-            map[i] = new Array(maxY).fill(0);
-        }
-        for (let i = 0; i < all[2]; i++) {
-            let a = arr.shift().split(' ');
-            map[a[0]][a[1]] = 1;
-        }
-    }
-
-    makeMap(inputSan);
-
-    function BFS(startX, startY) {
-        let dx = [1, -1, 0, 0];
-        let dy = [0, 0, 1, -1];
-        let queue = [[startX, startY]];
-
-        while (queue.length > 0) {
-            let [x, y] = queue.shift();
-            visited[x][y] = true;
-            for (let i = 0; i < dx.length; i++) {
-                let nextX = x + dx[i];
-                let nextY = y + dy[i];
-                if (nextX >= 0 && nextX < maxX && nextY >= 0 && nextY < maxY && map[nextX][nextY] === 1 && visited[nextX][nextY] === false) {
-                    queue.push([nextX, nextY]);
-                }
-            }
-
-        }
-
-    }
-
-// BFS(0, 0, 0);
-    function countingFun() {
-        let count = 0;
-        for (let i = 0; i < maxX; i++) {
-            for (let j = 0; j < maxY; j++) {
-                if (visited[i][j] === false && map[i][j] === 1) {
-                    BFS(i, j);
-                    count++;
-                }
+function DFS(X, Y) {
+    let dx = [1, -1, 0, 0];
+    let dy = [0, 0, 1, -1];
+    map[X][Y] = 0;
+    for (let i = 0; i < dx.length; i++) {
+        const nextX = X + dx[i];
+        const nextY = Y + dy[i];
+        if (nextY < SIZE[1] && nextX < SIZE[0] && nextX >= 0 && nextY >= 0){
+            if (map[nextX][nextY] === 1) {
+                DFS(nextX, nextY);
             }
         }
-        return count;
     }
-
-    console.log(countingFun());
 }
 
-for (let i = 0; i < testNUM; i++) {
-    main(input);
+for (let i = 0; i < N; i++) {
+    let Answer = 0;
+    var SIZE = input.shift().split(' ').map(Number);
+
+    var map = new Array(SIZE[0]);
+    for (let i = 0; i < map.length; i++) {
+        map[i] = new Array(SIZE[1]).fill(0);
+
+    }
+    for (let i = 0; i < SIZE[2]; i++) {
+        const [LocX, LocY] = input.shift().split(" ").map(Number);
+        map[LocX][LocY] = 1;
+    }
+    for (let i = 0; i < SIZE[0]; i++) {
+        for (let j = 0; j < SIZE[1]; j++) {
+            if (map[i][j] === 1) {
+                DFS(i, j);
+                Answer++;
+            }
+        }
+    }
+    console.log(Answer);
 }
