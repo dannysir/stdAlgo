5770b9d9b1353bc0a27c2531685ddd6dbbd093b5
Seo San <dannysir@naver.com>
2024-02-23 13:00:56 +0900

    feat : combination 구현

--- index.js
+++ index.js
@@ -7,64 +7,82 @@ let An = input.shift().split(' ').map(Number);
 const CalArr = input.shift().split(' ').map(Number);
 let MaxStack = [];
 let MinStack = [];
-const MakeMaxStack = (ARR) => {
-
-    for (let i = 0; i < ARR[1]; i++) {
-        MaxStack.push('-');
+console.log(CalArr);
+const MakeCombination = (Arr, RESULT, INDEX) => {
+    let [PLUS, MINUS, MULTI, DIVID] = Arr;
+    if (INDEX === An.length - 1) {
+        MaxStack.push(RESULT);
+        return;
     }
-    for (let i = 0; i < ARR[3]; i++) {
-        MaxStack.push('/');
+    const NextIndex = INDEX + 1;
+    if (PLUS > 0) {
+        MakeCombination([PLUS - 1,MINUS,MULTI,DIVID], RESULT + '+', NextIndex);
     }
-    for (let i = 0; i < ARR[0]; i++) {
-        MaxStack.push('+');
+    if (MINUS > 0) {
+        MakeCombination([PLUS,MINUS - 1,MULTI,DIVID], RESULT + '-', NextIndex);
     }
-    for (let i = 0; i < ARR[2]; i++) {
-        MaxStack.push('*');
+    if (MULTI > 0) {
+        MakeCombination([PLUS,MINUS,MULTI - 1,DIVID], RESULT + '*', NextIndex);
     }
-}
-
-const MakeMinStack = (ARR) => {
-    for (let i = 0; i < ARR[0]; i++) {
-        MinStack.push('+');
-    }
-    for (let i = 0; i < ARR[3]; i++) {
-        MinStack.push('/');
-    }
-    for (let i = 0; i < ARR[1]; i++) {
-        MinStack.push('-');
-    }
-    for (let i = 0; i < ARR[2]; i++) {
-        MinStack.push('*');
+    if (DIVID > 0) {
+        MakeCombination([PLUS,MINUS,MULTI,DIVID - 1], RESULT + '/', NextIndex);
     }
 }
-
-MakeMaxStack(CalArr);
-MakeMinStack(CalArr);
-console.log(An);
+MakeCombination(CalArr, '', 0);
 console.log(MaxStack);
-console.log(MinStack);
+console.log(An);
+// const MakeMaxStack = (ARR) => {
+//
+//     for (let i = 0; i < ARR[1]; i++) {
+//         MaxStack.push('-');
+//     }
+//     for (let i = 0; i < ARR[3]; i++) {
+//         MaxStack.push('/');
+//     }
+//     for (let i = 0; i < ARR[0]; i++) {
+//         MaxStack.push('+');
+//     }
+//     for (let i = 0; i < ARR[2]; i++) {
+//         MaxStack.push('*');
+//     }
+// }
+//
+// const MakeMinStack = (ARR) => {
+//     for (let i = 0; i < ARR[0]; i++) {
+//         MinStack.push('+');
+//     }
+//     for (let i = 0; i < ARR[3]; i++) {
+//         MinStack.push('/');
+//     }
+//     for (let i = 0; i < ARR[1]; i++) {
+//         MinStack.push('-');
+//     }
+//     for (let i = 0; i < ARR[2]; i++) {
+//         MinStack.push('*');
+//     }
+// }
 
-const MathAnswer = (NumArr, OperatorArr) => {
-    let result = 0;
-    let tmp = NumArr[NumArr.length - 1];
-    for (let i = OperatorArr.length - 1; i >= 0 ; i--) {
-        if (OperatorArr[i] === '*') {
-            tmp = tmp * NumArr[i];
-        }
-        if (OperatorArr[i] === '+') {
-            result += tmp;
-            tmp = NumArr[i];
-        }
-        if (OperatorArr[i] === '/') {
-            tmp = Math.floor(NumArr[i] / tmp);
-        }
-        if (OperatorArr[i] === '-') {
-            result -= tmp;
-            tmp = NumArr[i];
-        }
-    }
-    result += tmp;
-    console.log(result);
-};
-MathAnswer(An, MaxStack);
-MathAnswer(An, MinStack);
+// const MathAnswer = (NumArr, OperatorArr) => {
+//     let result = 0;
+//     let tmp = NumArr[NumArr.length - 1];
+//     for (let i = OperatorArr.length - 1; i >= 0 ; i--) {
+//         if (OperatorArr[i] === '*') {
+//             tmp = tmp * NumArr[i];
+//         }
+//         if (OperatorArr[i] === '+') {
+//             result += tmp;
+//             tmp = NumArr[i];
+//         }
+//         if (OperatorArr[i] === '/') {
+//             tmp = Math.floor(NumArr[i] / tmp);
+//         }
+//         if (OperatorArr[i] === '-') {
+//             result -= tmp;
+//             tmp = NumArr[i];
+//         }
+//     }
+//     result += tmp;
+//     console.log(result);
+// };
+// MathAnswer(An, MaxStack);
+// MathAnswer(An, MinStack);
