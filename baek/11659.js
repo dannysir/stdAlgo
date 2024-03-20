50d4519c3693cb4a36243c995f7acec2ca59f222
Seo San <dannysir@naver.com>
2024-03-20 12:23:14 +0900

    누적합

---
+++ baek/11659.js
@@ -0,0 +1,20 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input.shift().split(' ').map(Number);
+const Numbers = input.shift().split(' ').map(Number);
+input = input.map(v => v.split(' ').map(Number));
+let AccNum = new Array(N + 1).fill(0);
+let answer = [];
+
+for (let i = 1; i < AccNum.length; i++) {
+    AccNum[i] += AccNum[i - 1] + Numbers[i - 1];
+}
+const solution = (INPUT) => {
+    for (const inputElement of INPUT) {
+        const [Start, End] = inputElement;
+        answer.push(AccNum[End] - AccNum[Start - 1]);
+    }
+    console.log(answer.join('\n'));
+};
+solution(input);
