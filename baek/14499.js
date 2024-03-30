668271d9f8ca34f841bcb072a59114558b4284c2
Seo San <dannysir@naver.com>
2024-03-30 10:37:25 +0900

    refactor : 메인함수 수정 반복되는 로직 모듈화

--- baek/14499.js
+++ baek/14499.js
@@ -5,14 +5,9 @@ let input = fs.readFileSync("./input.text").toString().trim().split('\n');
 let [N, M, X, Y, OrderCount] = input.shift().split(' ').map(Number);
 let map = input.splice(0, N).map(v => v.split(' ').map(Number));
 const Orders = input.shift().split(' ').map(Number);
-// let Dice = new Array(2);
-// // 0 => 1, 3 => 0
-// Dice[0] = new Array(4);
-// // 1 => [0][1], [0][3]
-// Dice[1] = new Array(4);
 
 class Dice {
-    constructor(nowX,nowY) {
+    constructor(nowX, nowY) {
         this.up = 0;
         this.down = 0;
         this.left = 0;
@@ -24,26 +19,55 @@ class Dice {
     }
 
     GoDown() {
-        [this.up, this.down, this.left, this.right, this.front, this.back] = [this.back, this.front, this.left, this.right, this.up, this.down];
-        this.x++;
-        this.ChangeValue();
+        const [nextX, nextY] = [this.x + 1, this.y];
+        if (this.CheckOut(nextX, nextY)) {
+            [this.up, this.down, this.front, this.back] = [this.back, this.front, this.up, this.down];
+            this.x = nextX;
+            this.ChangeValue();
+            return this.up;
+        } else {
+            return -1;
+        }
     }
 
     GoUp() {
-        [this.up, this.down, this.left, this.right, this.front, this.back] = [this.front, this.back, this.left, this.right, this.down, this.up];
-        this.x--;
-        this.ChangeValue();
+        const [nextX, nextY] = [this.x - 1, this.y];
+        if (this.CheckOut(nextX, nextY)) {
+
+            [this.up, this.down, this.front, this.back] = [this.front, this.back, this.down, this.up];
+            this.x = nextX;
+            this.ChangeValue();
+            return this.up;
+
+        }else {
+            return -1;
+        }
     }
 
     GoLeft() {
-        [this.up, this.down, this.left, this.right, this.front, this.back] = [this.left, this.right, this.down, this.up, this.front, this.back];
-        this.y--;
-        this.ChangeValue();
+        const [nextX, nextY] = [this.x, this.y - 1];
+        if (this.CheckOut(nextX, nextY)) {
+            [this.up, this.down, this.left, this.right] = [this.left, this.right, this.down, this.up];
+            this.y = nextY;
+            this.ChangeValue();
+            return this.up;
+
+        }else {
+            return -1;
+        }
     }
+
     GoRight() {
-        [this.up, this.down, this.left, this.right, this.front, this.back] = [this.right, this.left, this.up, this.down, this.front, this.back];
-        this.y++;
-        this.ChangeValue();
+        const [nextX, nextY] = [this.x, this.y + 1];
+        if (this.CheckOut(nextX, nextY)) {
+            [this.up, this.down, this.left, this.right] = [this.right, this.left, this.up, this.down];
+            this.y = nextY;
+            this.ChangeValue();
+            return this.up;
+
+        }else {
+            return -1;
+        }
     }
 
     ChangeValue() {
@@ -56,53 +80,27 @@ class Dice {
         }
     }
 
-    GetPosition() {
-        return [this.x, this.y];
+    CheckOut(nextX, nextY) {
+        return !(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M);
     }
-
-    GetUpside() {
-        return this.up;
-    }
-}
-
-const CheckOut = (nextX, nextY) => {
-    if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
-        return false;
-    }
-    return true;
 }
 const solution = () => {
     const MyDice = new Dice(X, Y);
     let answer = [];
     for (const order of Orders) {
+        let goDice = -1;
         if (order === 1) {
-            let [nextX, nextY] = MyDice.GetPosition();
-            nextY += 1;
-            if (CheckOut(nextX, nextY)) {
-                MyDice.GoRight();
-                answer.push(MyDice.GetUpside());
-            }
-        }else if (order === 2) {
-            let [nextX, nextY] = MyDice.GetPosition();
-            nextY -= 1;
-            if (CheckOut(nextX, nextY)) {
-                MyDice.GoLeft();
-                answer.push(MyDice.GetUpside());
-            }
-        }else if (order === 3) {
-            let [nextX, nextY] = MyDice.GetPosition();
-            nextX -= 1;
-            if (CheckOut(nextX, nextY)) {
-                MyDice.GoUp();
-                answer.push(MyDice.GetUpside());
-            }
-        }else if (order === 4) {
-            let [nextX, nextY] = MyDice.GetPosition();
-            nextX += 1;
-            if (CheckOut(nextX, nextY)) {
-                MyDice.GoDown();
-                answer.push(MyDice.GetUpside());
-            }
+            goDice = MyDice.GoRight();
+        } else if (order === 2) {
+            goDice = MyDice.GoLeft();
+        } else if (order === 3) {
+            goDice = MyDice.GoUp();
+        } else if (order === 4) {
+            goDice = MyDice.GoDown();
+        }
+
+        if (goDice !== -1) {
+            answer.push(goDice);
         }
     }
     console.log(answer.join('\n'));
