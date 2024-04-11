56c209df5600e0e629a4cc1f7c0ff6cc53ee16f4
Seo San <dannysir@naver.com>
2024-04-11 17:59:36 +0900

    BFS, 구현

---
+++ baek/2636.js
@@ -0,0 +1,60 @@
+let fs = require("fs");
+let input = fs.readFileSync("./input.text").toString().trim().split('\n');
+
+// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let [N, M] = input.shift().split(' ').map(Number);
+let Map = input.map(v => v.split(' ').map(Number));
+const BFS = (x, y) => {
+    let visited = Array.from({length: N}, _ => Array(M).fill(false));
+    let Queue = [[x, y]];
+    visited[x][y] = true;
+    let idx = 0;
+    let dx = [1, -1, 0, 0];
+    let dy = [0, 0, 1, -1];
+    while (Queue.length > idx) {
+        let [X, Y] = Queue[idx];
+        for (let i = 0; i < dx.length; i++) {
+            const NextX = X + dx[i];
+            const NextY = Y + dy[i];
+            if (NextX < 0 || NextX >= N || NextY < 0 || NextY >= M) continue;
+            if (!visited[NextX][NextY]) {
+                if (Map[NextX][NextY] === 0) {
+                    visited[NextX][NextY] = true;
+                    Queue.push([NextX, NextY]);
+                }else if (Map[NextX][NextY] === 1) {
+                    visited[NextX][NextY] = true;
+                    Map[NextX][NextY] = 2;
+                }
+            }
+
+        }
+        idx++;
+    }
+};
+const Rotten = () => {
+    let CheeseExist = 0;
+    let RottenCheese = 0;
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++) {
+            if (Map[i][j] === 1) {
+                CheeseExist++;
+            } else if (Map[i][j] === 2) {
+                Map[i][j] = 0;
+                RottenCheese++;
+            }
+        }
+    }
+    return [CheeseExist, RottenCheese];
+};
+
+const solution = () => {
+    let timer = 0;
+    let [CheeseExist, RottenCheese] = Rotten();
+    while (CheeseExist > 0) {
+        BFS(0, 0);
+        [CheeseExist, RottenCheese] = Rotten();
+        timer++;
+    }
+    console.log(`${timer}\n${RottenCheese}`);
+};
+solution();
