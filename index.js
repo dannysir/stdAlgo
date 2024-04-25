ded7c9213f61cd860122c44b7021234e1587f1e5
Seo San <dannysir@naver.com>
2024-04-25 21:45:14 +0900

    16507 하는중

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let fs = require("fs");
-let input = fs.readFileSync("./input.text").toString().trim().split('\n');
+let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+//let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M, Q] = input.shift().split(' ').map(Number);
+const Picture = input.splice(0, N).map(v => v.split(' ').map(Number));
+input = input.map(v => v.split(' ').map(Number));
 
-let obj1 = {
-    1: 'a',
-    2: 'b',
-    3: 'c',
-    length: 3
+let Board = Array.from({length: N + 1}, _ => Array(M + 1).fill(0));
+
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+        Board[i + 1][j + 1] = Picture[i][j] + Board[i][j + 1] + Board[i + 1][j] - Board[i][j];
+    }
+
 }
-Array.prototype.push.call(obj1, 'd');
-console.log(obj1);
+console.log(Picture);
+console.log(Board);
