faa75a7815b4848c7f987baf8ba9cf8dada456a9
Seo San <dannysir@naver.com>
2024-05-02 17:16:31 +0900

    DP

---
+++ baek/11727.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+//let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+let N = parseInt(input.shift());
+let dp = new Array(10001).fill(-1);
+dp[1] = 1;
+dp[2] = 3;
+for (let i = 3; i < dp.length; i++) {
+    dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % 10007;
+}
+console.log(dp[N]);
