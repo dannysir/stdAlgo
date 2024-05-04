ca0a39d140f76ce334445ebe5654215a12d8f5e0
Seo San <dannysir@naver.com>
2024-05-04 22:52:12 +0900

    no message

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.text").toString().trim().split("\n");
 //let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-
-const [N, M, Q] = input.shift().split(' ').map(Number);
-const Picture = input.splice(0, N).map(v => v.split(' ').map(Number));
-input = input.map(v => v.split(' ').map(Number));
-
-let Board = Array.from({length: N + 1}, _ => Array(M + 1).fill(0));
-
-for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-        Board[i + 1][j + 1] = Picture[i][j] + Board[i][j + 1] + Board[i + 1][j] - Board[i][j];
-    }
-
+let N = parseInt(input.shift());
+let dp = new Array(10001).fill(-1);
+dp[1] = 1;
+dp[2] = 3;
+for (let i = 3; i < dp.length; i++) {
+    dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % 10007;
 }
-console.log(Picture);
-console.log(Board);
+console.log(dp[N]);
