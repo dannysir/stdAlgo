f10f1d6a7c899e3ec03e497d695fb653c8d7c793
Seo San <dannysir@naver.com>
2024-06-14 16:07:01 +0900

    node readline 연습

--- baek/10799.js
+++ baek/10799.js
@@ -21,3 +21,39 @@ for (let i = 0; i < input.length; i++) {
     }
 }
 console.log(answer);
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+//
+// let input = [];
+// let stack = [];
+// let isLaser = false;
+// const solution = (Arr) => {
+//     let cnt = 0;
+//     for (const bracket of Arr) {
+//         if (bracket === "(") {
+//             isLaser = true;
+//             stack.push(bracket);
+//         } else {
+//             if (isLaser) {
+//                 stack.pop();
+//                 cnt += stack.length;
+//                 isLaser = false;
+//             } else {
+//                 stack.pop();
+//                 cnt += 1;
+//             }
+//         }
+//     }
+//     console.log(cnt);
+// };
+// rl.on("line", function (line) {
+//     input = line.split('');
+//     rl.close();
+// }).on("close", function () {
+//     solution(input);
+//     process.exit;
+// });
