95e1d4afe8f02ff25665bed1a0fd4ebbe92a2e9e
Seo San <dannysir@naver.com>
2024-06-21 18:37:55 +0900

    not finished

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let fs = require("fs");
 let input = fs.readFileSync("./input.text").toString().trim().split("\n");
-//let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-let N = parseInt(input.shift());
-let dp = new Array(10001).fill(-1);
-dp[1] = 1;
-dp[2] = 3;
-for (let i = 3; i < dp.length; i++) {
-    dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % 10007;
+// let input = require("fs").readFileSync(0, 'utf-8').toString().trim().split("\n");
+const N = parseInt(input.shift());
+const distance = input[0].split(' ').map(Number);
+const costs = input[1].split(' ').map(Number);
+
+let tank = BigInt(0);
+let answer = BigInt(0);
+for (let i = 0; i < costs.length - 1; i++) {
+    if (tank >= distance[i]) continue;
+
+
+    let tmp = BigInt(distance[i]);
+    for (let j = i + 1; j < costs.length - 1; j++) {
+        if (costs[j] < costs[i]) break;
+
+        tmp += BigInt(distance[j]);
+    }
+    tank += tmp;
+    answer += tmp * BigInt(costs[i]);
+
 }
-console.log(dp[N]);
+console.log(String(answer));
