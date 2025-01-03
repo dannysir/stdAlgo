6f00514aae084de5da96ffaaf20ecb398c98f3f3
Seo San <dannysir@naver.com>
2025-01-03 15:13:39 +0900

    이분탐색

---
+++ baek/2805.js
@@ -0,0 +1,30 @@
+let fs = require("fs");
+let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+// let input = require("fs").readFileSync(0, 'utf-8').toString().trim().split("\n");
+
+const [N, M] = input.shift().split(' ').map(Number);
+const trees = input.shift().split(' ').map(Number);
+trees.sort((a, b) => a - b);
+
+let left = 0;
+let right = trees[trees.length - 1];
+let answer = 0;
+
+while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let sum = 0;
+
+    for (let tree of trees) {
+        if (tree > mid) {
+            sum += tree - mid;
+        }
+    }
+
+    if (sum >= M) {
+        answer = mid;
+        left = mid + 1;
+    } else {
+        right = mid - 1;
+    }
+}
+console.log(answer);
