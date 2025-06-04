3615b5f28cd1588518458f2cc05f9db9e0cb662a
Seo San <dannysir@naver.com>
2025-06-04 16:41:18 +0900

    bfs, 구현

--- baek/14502.js
+++ baek/14502.js
@@ -1,60 +1,63 @@
 let fs = require("fs");
-let input = fs.readFileSync("./input.text").toString().trim().split('\n');
+let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+// let input = require("fs").readFileSync(0, 'utf-8').toString().trim().split("\n");
 
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-
-let [N, M] = input.shift().split(' ').map(Number);
-let MAP = input.map(v => v.split(' ').map(Number));
-const Find = (map, target) => {
-    let Queue = [];
+const [N, M] = input.shift().split(' ').map(Number);
+const boards = input.map(v => v.split(' ').map(Number));
+const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+const find = (board, target) => {
+    const queue = [];
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < M; j++) {
-            if (map[i][j] === target) {
-                Queue.push([i, j]);
+            if (board[i][j] === target) {
+                queue.push([i, j]);
             }
         }
     }
-    return Queue;
+    return queue;
 };
-const BFS = (map) => {
-    let Queue = Find(map, 2);
+const bfs = (board) => {
+    const queue = find(board, 2);
     let idx = 0;
-    let dx = [1, -1, 0, 0];
-    let dy = [0, 0, 1, -1];
-    while (Queue.length > idx) {
-        const [x, y] = Queue[idx];
-        for (let i = 0; i < dx.length; i++) {
-            const NextX = x + dx[i];
-            const NextY = y + dy[i];
-            if (NextX >= 0 && NextY >= 0 && NextX < N && NextY < M) {
-                if (map[NextX][NextY] === 0) {
-                    map[NextX][NextY] = 2;
-                    Queue.push([NextX, NextY]);
-                }
+    while (queue.length > idx) {
+        const [x, y] = queue[idx++];
+
+        for (const [dx, dy] of dirs) {
+            const nx = x + dx;
+            const ny = y + dy;
+
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+            if (board[nx][ny] === 0) {
+                board[nx][ny] = 2;
+                queue.push([nx, ny]);
             }
         }
-        idx++;
     }
-    return Find(map, 0).length;
+
+    return find(board, 0).length;
 };
 
-let max = 0;
-const DFS = (wall) => {
-    if (wall === 3) {
-        const newMap = MAP.map(v => [...v]);
-        let answer = BFS(newMap);
-        max = Math.max(answer, max);
+let answer = 0;
+
+const combination = (cnt) => {
+    if (cnt === 3) {
+        const copyBoard = boards.map(v => [...v]);
+        const size = bfs(copyBoard);
+        answer = Math.max(answer, size);
         return;
     }
+
     for (let i = 0; i < N; i++) {
         for (let j = 0; j < M; j++) {
-            if (MAP[i][j] === 0) {
-                MAP[i][j] = 1;
-                DFS(wall + 1);
-                MAP[i][j] = 0;
+            if (boards[i][j] === 0) {
+                boards[i][j] = 1;
+                combination(cnt + 1);
+                boards[i][j] = 0;
             }
         }
     }
-};
-DFS(0);
-console.log(max);
+}
+combination(0);
+
+console.log(answer);
