a4ce7512750cdf88c02259d7f84c8b6dcf551fff
Seo San <dannysir@naver.com>
2025-07-28 12:33:24 +0900

    단조 스택

--- baek/6198.js
+++ baek/6198.js
@@ -1,34 +1,20 @@
 let fs = require("fs");
-let input = fs.readFileSync("./input.text").toString().trim().split("\n").map(Number);
-// let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-let n = input.shift();
-let Stack = [];
+let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+// let input = require("fs").readFileSync(0, 'utf-8').toString().trim().split("\n");
+const n = input.shift();
+input = input.map(Number);
+const stack = [];
 let answer = 0;
+
 for (let i = 0; i < input.length; i++) {
-    while (Stack.length) {
-        if (Stack[Stack.length - 1] <= input[i]) {
-            Stack.pop();
+    while (stack.length) {
+        if (stack[stack.length - 1] < input[i]) {
+            stack.pop();
         }else break;
     }
-    Stack.push(input[i]);
-    answer += Stack.length - 1;
+
+    stack.push(input[i]);
+    answer += stack.length - 1;
 }
 
 console.log(answer);
-// let answer = 0;
-//
-// input.forEach((Value, Index) => {
-//     let cnt = 0;
-//     if (Index !== input.length - 1) {
-//         for (let i = Index + 1; i < input.length; i++) {
-//             if (Value > input[i]) {
-//                 cnt++;
-//             }else break;
-//         }
-//         answer += cnt;
-//     }else {
-//         answer += cnt;
-//     }
-//
-// });
-// console.log(answer);
