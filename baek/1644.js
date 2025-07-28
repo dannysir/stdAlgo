7737fc49366075539e42d1c45b0270870f2527dd
Seo San <dannysir@naver.com>
2025-07-28 13:09:23 +0900

    에라토스테네스의 체 & 투포인터

--- baek/1644.js
+++ baek/1644.js
@@ -33,3 +33,48 @@ while (right <= PrimeNumber.length) {
     }
 }
 console.log(cnt);
+//
+// let fs = require("fs");
+// let input = fs.readFileSync("./input.text").toString().trim().split("\n");
+// // let input = require("fs").readFileSync(0, 'utf-8').toString().trim().split("\n");
+//
+// const N = +input.shift();
+//
+// let primeNumberArray = Array.from({length: N + 1}, (_, index) => index);
+//
+// for (let i = 2; i * i < primeNumberArray.length; i++) {
+//     if (primeNumberArray[i] === i) {
+//         for (let j = i * i; j < primeNumberArray.length; j += i) {
+//             if (primeNumberArray[j] === j && j % i === 0) {
+//                 primeNumberArray[j] = i;
+//             }
+//         }
+//     }
+// }
+//
+// primeNumberArray = primeNumberArray.filter((value, index) => value > 1 && value === index);
+//
+// let left = 0;
+// let right = left;
+// let sum = primeNumberArray[left];
+// let answer = 0;
+//
+// while (right < primeNumberArray.length && left <= right) {
+//     if (sum === N) {
+//         answer++;
+//         right++;
+//         if (primeNumberArray[right]) {
+//             sum += primeNumberArray[right];
+//         }
+//         continue;
+//     }
+//
+//     if (sum < N) {
+//         right++;
+//         sum += primeNumberArray[right];
+//     }else if (sum > N) {
+//         sum -= primeNumberArray[left];
+//         left++;
+//     }
+// }
+// console.log(answer);
